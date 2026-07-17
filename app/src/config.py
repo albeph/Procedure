7ba12eb0ead4ed473e4f1ee937cca493002e3ab0
albeph <albeph.ph@gmail.com>
2026-07-17 23:11:58 +0200

    chore: bump version to 1.0.4 and stage user comments

--- app/src/config.py
+++ app/src/config.py
@@ -11,7 +11,7 @@ if "-v" in sys.argv:
     sys.argv.remove("-v")
 
 # Globals Configuration Constants
-VERSION = "1.0.3"
+VERSION = "1.0.4"
 CONFIG_DIR = os.path.expanduser("~/.config/procedure")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "config.json")
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15"
